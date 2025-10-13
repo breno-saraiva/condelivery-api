00a3c9b42e0581f7c06eb75ceedd36cd9a856b2f
breno-saraiva <brenodev.saraiva@gmail.com>
2025-10-13 16:33:14 -0300

    feat: creation of Middleware for auth routes

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -4,8 +4,8 @@ import { prisma } from '../prismaClient';
 
 export interface JwtPayloadCustom {
   tipo_usuario: 'morador' | 'condominio';
-  nome_usuario?: string;
-  email_usuario?: string;
+  nome_usuario: string;
+  email_usuario: string;
   iat?: number;
   exp?: number;
 }
@@ -20,21 +20,18 @@ export const autenticarToken = async (req: Request, res: Response, next: NextFun
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET as string) as JwtPayloadCustom;
 
-    // opcional: buscar usuário no DB para garantir que ainda existe
-    if (payload.role === 'morador') {
-      const morador = await prisma.morador.findUnique({ where: { id: payload.sub } });
+    if (payload.tipo_usuario === 'morador') {
+      const morador = await prisma.morador.findUnique({ where: { email: payload.email_usuario } });
       if (!morador) return res.status(401).json({ message: 'Usuário não encontrado' });
-
-      req.user = { id: morador.id, role: 'morador', nome: morador.nome, email: morador.email };
     } else {
-      const cond = await prisma.condominio.findUnique({ where: { id: payload.sub } });
+      const cond = await prisma.condominio.findUnique({
+        where: { emailAdm: payload.email_usuario },
+      });
       if (!cond) return res.status(401).json({ message: 'Usuário não encontrado' });
-
-      req.user = { id: cond.id, role: 'condominio', nome: cond.nomeAdm, email: cond.emailAdm };
     }
 
     next();
-  } catch (err) {
+  } catch {
     return res.status(401).json({ message: 'Token inválido ou expirado' });
   }
 };
