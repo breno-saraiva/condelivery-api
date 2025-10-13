c16d04f1c61b4a447ee0a4e88600e832be6e55f8
breno-saraiva <brenodev.saraiva@gmail.com>
2025-10-13 16:29:36 -0300

    feat: creation of entity dto

--- src/types/Dto.ts
+++ src/types/Dto.ts
@@ -1,16 +1,43 @@
-export type CreateCondominioDto = {
+export type CondominioDto = {
   nome: string;
-  cnpj?: string;
-  logradouro?: string;
-  numero?: string;
-  complemento?: string;
-  bairro?: string;
-  estado?: string;
-  municipio?: string;
-  cep?: string;
-  nomeAdm?: string;
-  cpfAdm?: string;
-  emailAdm?: string;
-  celularAdm?: string;
-  senhaAdm?: string;
+  cnpj: string;
+  logradouro: string;
+  numero: string;
+  complemento: string;
+  bairro: string;
+  estado: string;
+  municipio: string;
+  cep: string;
+  nomeAdm: string;
+  cpfAdm: string;
+  emailAdm: string;
+  celularAdm: string;
+  senhaAdm: string;
+  moradores: MoradorDto[];
+};
+
+export type MoradorDto = {
+  id: number;
+  nome: string;
+  cpf: string;
+  celular: string;
+  email: string;
+  dataNascimento?: string;
+  unidade: string;
+  ehEntragador: boolean;
+  senha: string;
+  condominioId: number;
+  condominio: CondominioDto;
+  pedidos: PedidosDto[];
+};
+
+export type PedidosDto = {
+  id: number;
+  plataforma: string;
+  descricao: string;
+  previsaoChegada?: Date;
+  localEntrega: string;
+  status: string;
+  moradorId: number;
+  morador: MoradorDto;
 };
